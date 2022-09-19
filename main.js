ad4222134918ec398104c723ff66e6115c0fe39d
Camila Omenna <112710301+CamilaOmenna@users.noreply.github.com>
2022-09-18 23:16:21 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+const Nome = prompt (' Qual nome do paciente ??');
+console.log ('')
+
+const Peso = prompt ('Qual é o peso do paciente??')
+
+console.log (' ')
+
+const Altura = prompt ('Qual é altura do paciente ?')
+
+console.log ('')
+
+
+const Internação = prompt ('Que dia será a Data de Internação? ')
+console.log ('')
+
+ const Hipertensão = prompt (' O paciente é Diabético ?')
+console.log ('')
